fe309d394b8f915c8b055e9c4963815a280ba0f5
Daniel Kontsek <daniel@kontsek.sk>
2017-04-09 15:03:36 +0200

    Updated Erigones banner

--- usr/src/uts/common/os/logsubr.c
+++ usr/src/uts/common/os/logsubr.c
@@ -251,7 +251,7 @@ log_init(void)
 	printf("\rSunOS Release %s Version %s %u-bit\n",
 	    utsname.release, utsname.version, NBBY * (uint_t)sizeof (void *));
 	printf("Copyright (c) 2010-2016, Joyent Inc. All rights reserved.\n");
-	printf("Copyright (c) 2016, Erigones, s. r. o. All rights reserved.\n");
+	printf("Copyright (c) 2016-2017, Erigones, s. r. o. All rights reserved.\n");
 #ifdef DEBUG
 	printf("DEBUG enabled\n");
 #endif
